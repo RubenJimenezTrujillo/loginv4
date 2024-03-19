43ddc043699ccf2d0940a8ec707da71203704c23
RubenJT <mexicanopromedioxd@gmail.com>
2024-03-18 21:05:46 -0600

    app funcional sin errores aparentes se agregaron mas validaciones importante falta  metodos de seguridad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.android.gms:play-services-measurement-api:21.5.1")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/loginv4/MainActivity.kt
+++ app/src/main/java/com/example/loginv4/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.loginv4
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,44 +12,67 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var firebaseAuth:FirebaseAuth
-    private  lateinit var auth: FirebaseAuth.AuthStateListener
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth.AuthStateListener
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btningresar : Button = findViewById(R.id.ButtonAccess)
-        val btnadd : Button = findViewById(R.id.ButtonAdd)
-        val emailtxt : TextView =findViewById(R.id.emailText)
-        val passtxt : TextView = findViewById(R.id.passwortText)
+        val btningresar: Button = findViewById(R.id.ButtonAccess)
+        val btnadd: Button = findViewById(R.id.ButtonAdd)
+        val emailtxt: TextView = findViewById(R.id.emailText)
+        val passtxt: TextView = findViewById(R.id.passwortText)
         firebaseAuth = Firebase.auth
-        btningresar.setOnClickListener(){
-            singIn(emailtxt.text.toString(),passtxt.text.toString())
+        btningresar.setOnClickListener() {
+            if(emailtxt.text.isNotEmpty() && passtxt.text.isNotEmpty()){
+                singIn(emailtxt.text.toString(), passtxt.text.toString())
+            }
+            else{
+                Toast.makeText(baseContext, "Error de email y/o contrasena esta en blanco", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+
+
+
         }
 
     }
-    private fun singIn(email:String , password:String) {
 
-        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this) {
+    private fun singIn(email: String, password: String) {
 
-            task ->
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
 
-            if (task.isSuccessful){
+                task ->
+
+
+            if (task.isSuccessful) {
                 val user = firebaseAuth.currentUser
-                Toast.makeText(baseContext,user?.uid.toString(),Toast.LENGTH_SHORT).show()
+                Toast.makeText(baseContext, user?.uid.toString(), Toast.LENGTH_SHORT).show()
 
-            }else {
-                Toast.makeText(baseContext,"Error de email y/o contrasena",Toast.LENGTH_SHORT).show()
+                val i = Intent(this, inicio::class.java)
+                startActivity(i)
+            } else {
+                Toast.makeText(baseContext, "Error de email y/o contrasena", Toast.LENGTH_SHORT)
+                    .show()
             }
 
         }
 
 
+    }
+
+
+
+
+
+
         }
 
-    }
+
+
 
 
 
